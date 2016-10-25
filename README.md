305e32b3f7cd79958c4d361c0d79a60acbb19fc0
Jason Shaffer <Jason Shaffer>
2016-10-25 18:42:33 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SELCAdventures
+Code Repository for the Adventures of Jack's Software Engineering Learning Community Group.
---
+++ main.py
@@ -0,0 +1,5 @@
+from unicurses import *
+
+stdscr = initscr() # initializes the standard screen
+addstr("Jack is not the best")
+getch()
